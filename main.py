8fcd6720f960a84694dd5a22d73641483517e962
Leafurya <audrb5300@naver.com>
2025-04-04 22:53:53 +0900

    start

---
+++ main.py
@@ -0,0 +1,81 @@
+import sys
+import spacy
+import sqlite3
+import hashlib
+import os
+    
+if getattr(sys, 'frozen', False):  # PyInstaller로 빌드된 경우
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+    
+if __name__=="__main__":
+    model_path = os.path.join(base_path, "model", "en_core_web_sm", "en_core_web_sm-3.7.1")
+    nlp=spacy.load(model_path)
+    text=sys.argv[1]
+    doc=nlp(text)
+    result=doc.to_json()
+    
+    conn=sqlite3.connect("database.db")
+    cur=conn.cursor()
+    
+    try:
+        cur.execute("""
+                CREATE TABLE "texts" (
+                    "textid"	INTEGER PRIMARY KEY AUTOINCREMENT,
+                    "text"	TEXT NOT NULL,
+                    "hash"	TEXT UNIQUE NOT NULL
+                )
+            """)
+        cur.execute("""
+                    CREATE TABLE parts (
+                        textid INTEGER NOT NULL,
+                        tokenid INTEGER,
+                        start INTEGER NOT NULL,
+                        end INTEGER,
+                        token TEXT,
+                        tag TEXT,
+                        pos TEXT,
+                        morph TEXT,
+                        lemma TEXT,
+                        dep TEXT,
+                        head INTEGER,
+                        PRIMARY KEY (textid, tokenid),
+                        FOREIGN KEY (textid) REFERENCES texts (textid)
+                    )
+                """)
+        conn.commit()
+    except  sqlite3.Error as e:{
+    }
+    
+    textid=0
+    hashed=hashlib.sha256(text.encode('utf-8')).hexdigest()
+    cur.execute("SELECT textid FROM texts WHERE hash = (?)",(hashed,))
+    for row in cur.fetchall():
+        print(row[0])
+        sys.exit()
+        
+    try:
+        cur.execute("""
+            INSERT INTO texts (text, hash) VALUES (?,?)
+        """,(text,hashed,))
+        conn.commit()
+        textid=cur.lastrowid
+    except sqlite3.Error as e:
+        print(f"SQLite 에러 발생: {e}")
+        print(-2)
+        sys.exit()
+    
+    # print(result["tokens"])
+    try:
+        for token in result["tokens"]:
+            cur.execute("""
+                            INSERT INTO parts VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                        """,(textid, token["id"],token["start"],token["end"], doc[token["id"]].text,token["tag"],token["pos"],token["morph"],token["lemma"],token["dep"],token["head"]))
+            conn.commit()
+    except sqlite3.Error as e:
+        print(f"SQLite 에러 발생: {e}")
+        print(-1)
+        sys.exit()
+        
+    print(textid)
